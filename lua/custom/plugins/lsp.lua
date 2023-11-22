d4fd335ad3a25ab6eebebed8f86983eb721cf5f4
Benjamin Valpey <bavalpey@users.noreply.github.com>
2023-11-22 12:36:39 -0600

    Added kebyinds

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,14 +1,27 @@
+local settings = {
+  format_on_save = false,
+  null_ls_settings = {
+    default_timeout = 2000,
+    diagnostics_format = "#{m} (#{s})",
+  },
+}
+
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = {"nvim-lua/plenary.nvim"},
-  config = function ()
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  config = function()
     require('null-ls').setup({
-      sources= {
+      default_timeout = 2000,
+      diagnostics_format = "#{m} (#{s})",
+      sources = {
         require('null-ls').builtins.formatting.black,
         require('null-ls').builtins.formatting.yamlfmt,
-        require('null-ls').builtins.formatting.yamllint,
-
+        --require('null-ls').builtins.formatting.yamllint,
       }
     })
   end,
+  init = function()
+    require('which-key').register({ ["<leader>cf"] = { function() vim.lsp.buf.format({ timeout_ms = settings
+      .null_ls_settings.default_timeout }) end, "Format/Fix" } })
+  end
 }
